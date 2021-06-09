4a0919822c6283b77770846b77080592d88fd879
slepher <cxyfree@gmail.com>
2021-06-09 10:26:22 +0800

    add copy_mod script

---
+++ copy_mod.sh
@@ -0,0 +1,7 @@
+rm -r ~/Documents/Paradox\ Interactive/Crusader\ Kings\ III/mod/gender_bug_fix_mod/
+rm -r ~/Documents/Paradox\ Interactive/Crusader\ Kings\ III/mod/locate_in_ds_tree_mod/
+rm -r ~/Documents/Paradox\ Interactive/Crusader\ Kings\ III/mod/forbes_world_top_remake_mod/
+cp -r gender_bug_fix_mod ~/Documents/Paradox\ Interactive/Crusader\ Kings\ III/mod/gender_bug_fix_mod
+cp -r locate_in_ds_tree_mod ~/Documents/Paradox\ Interactive/Crusader\ Kings\ III/mod/locate_in_ds_tree_mod
+cp -r forbes_world_top_remake_mod ~/Documents/Paradox\ Interactive/Crusader\ Kings\ III/mod/forbes_world_top_remake_mod
+
