b6bf2fa45ac6314878939bf83a678f3dd78ae649
slepher <slepheric@gmail.com>
2025-10-29 11:47:00 +0800

    update game files to 1.18

--- copy_game.sh
+++
@@ -1,5 +0,0 @@
-cp ~/Library/Application\ Support/Steam/steamapps/common/Crusader\ Kings\ III/game/gui/shared/cooltip.gui locate_in_ds_tree_mod/gui/shared/
-cp ~/Library/Application\ Support/Steam/steamapps/common/Crusader\ Kings\ III/game/gui/interaction_menu_window.gui locate_in_ds_tree_mod/gui/
-cp ~/Library/Application\ Support/Steam/steamapps/common/Crusader\ Kings\ III/game/gui/window_dynasty_tree.gui locate_in_ds_tree_mod/gui/
-cp ~/Library/Application\ Support/Steam/steamapps/common/Crusader\ Kings\ III/game/gui/hud.gui forbes_world_top_remake_mod/gui/
-cp ~/Library/Application\ Support/Steam/steamapps/common/Crusader\ Kings\ III/game/gui/window_character.gui gender_bug_fix_mod/gui/
